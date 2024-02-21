0461e9249497e81d58c30670c3581f4398d74d1a
aidanyang <aidanbyang@hotmail.com>
2024-02-20 22:09:10 -0500

    Add all time results

--- patches/generate_tbar_patches.py
+++ patches/generate_tbar_patches.py
@@ -3,12 +3,14 @@ import subprocess
 from colorama import Fore, Style
 import time
 import csv
+import argparse
 
 if __name__ == "__main__":
-    store_patches = False
-    use_ranked_patches = True
-    if store_patches:
-        use_ranked_patches = False
+    ap = argparse.ArgumentParser()
+    ap.add_argument("run_option", help="Options: store_patches, tbar or tbar_ranked")
+    args = ap.parse_args()
+    run_option = args.run_option
+
     current_path = os.getcwd()
     tbar_directory = f"{current_path}/TBar"
     patch_directory = f"{tbar_directory}/Results/PerfectFL/TBar/FixedBugs"
@@ -16,7 +18,6 @@ if __name__ == "__main__":
     output_partial = f"{tbar_directory}/OUTPUT/PerfectFL/TBar/PartiallyFixedBugs"
     output_unfix = f"{tbar_directory}/OUTPUT/PerfectFL/TBar/UnfixedBugs"
 
-    patch_done = []
     for subdir, _, files in os.walk(patch_directory):
         for file in files:
             proj_bug = subdir.split("/")[-1]
@@ -30,25 +31,31 @@ if __name__ == "__main__":
             project = proj_bug.split("_")[0]
             bug = proj_bug.split("_")[1]
 
-            if proj_bug in patch_done:
-                continue
-            # if "Lang_21" != proj_bug:
-            #     continue
-            patch_done.append(proj_bug)
+            if "tbar" in run_option:
+                if run_option == "tbar_ranked":
 
-            print(f"{Fore.GREEN}{Style.BRIGHT}Repairing {proj_bug}{Style.RESET_ALL}")
+                    time_file = "time_spent_entropy"
+                    checkout_command = f'mvn exec:java -Dexec.mainClass=edu.lu.uni.serval.tbar.main.Main -Dexec.args="-bugDataPath /home/TBar/bugdata -bugId {proj_bug} -d4jHome /home/defects4j/ -faultLocFile /home/TBar/BugPositions.txt -faultLocStrategy perfect -failedTests /home/TBar/FailedTestCases -recordAllPatches -patchRankFile /home/TBar/entropy_patch_rank.json" '
+                elif run_option == "tbar":
+                    time_file = "time_spent_tbar"
+                    checkout_command = f'mvn exec:java -Dexec.mainClass=edu.lu.uni.serval.tbar.main.Main -Dexec.args="-bugDataPath /home/TBar/bugdata -bugId {proj_bug} -d4jHome /home/defects4j/ -faultLocFile /home/TBar/BugPositions.txt -faultLocStrategy perfect -failedTests /home/TBar/FailedTestCases -recordAllPatches" '
 
-            if store_patches:
-                checkout_command = f'mvn exec:java -Dexec.mainClass=edu.lu.uni.serval.tbar.main.Main -Dexec.args="-bugDataPath /home/TBar/bugdata -bugId {proj_bug} -d4jHome /home/defects4j/ -faultLocFile /home/TBar/BugPositions.txt -faultLocStrategy perfect -failedTests /home/TBar/FailedTestCases -compileOnly -recordAllPatches -storePatchJson" '
-            elif use_ranked_patches:
-                time_file = "time_spent_entropy"
-                checkout_command = f'mvn exec:java -Dexec.mainClass=edu.lu.uni.serval.tbar.main.Main -Dexec.args="-bugDataPath /home/TBar/bugdata -bugId {proj_bug} -d4jHome /home/defects4j/ -faultLocFile /home/TBar/BugPositions.txt -faultLocStrategy perfect -failedTests /home/TBar/FailedTestCases -recordAllPatches -patchRankFile /home/TBar/entropy_patch_rank.json" '
+                # read entire file as string
+                with open(f"{current_path}/patches/{time_file}.csv", "r") as f:
+                    data = f.read()
+                    if f"{proj_bug}," in data:
+                        print(
+                            f"{Fore.RED}{Style.NORMAL}Already processed {proj_bug}{Style.RESET_ALL}"
+                        )
+                        continue
             else:
-                time_file = "time_spent_tbar"
-                checkout_command = f'mvn exec:java -Dexec.mainClass=edu.lu.uni.serval.tbar.main.Main -Dexec.args="-bugDataPath /home/TBar/bugdata -bugId {proj_bug} -d4jHome /home/defects4j/ -faultLocFile /home/TBar/BugPositions.txt -faultLocStrategy perfect -failedTests /home/TBar/FailedTestCases -recordAllPatches" '
+                checkout_command = f'mvn exec:java -Dexec.mainClass=edu.lu.uni.serval.tbar.main.Main -Dexec.args="-bugDataPath /home/TBar/bugdata -bugId {proj_bug} -d4jHome /home/defects4j/ -faultLocFile /home/TBar/BugPositions.txt -faultLocStrategy perfect -failedTests /home/TBar/FailedTestCases -compileOnly -recordAllPatches -storePatchJson" '
 
             start = time.time()
             try:
+                print(
+                    f"{Fore.GREEN}{Style.NORMAL}Running option: {run_option} for {proj_bug}{Style.RESET_ALL}"
+                )
                 output = subprocess.check_output(
                     checkout_command,
                     shell=True,
@@ -57,18 +64,16 @@ if __name__ == "__main__":
                 ).decode()
             except subprocess.TimeoutExpired as e:
                 print(
-                    f"{Fore.RED}{Style.BRIGHT}Timeout expired for {proj_bug}{Style.RESET_ALL}"
+                    f"{Fore.RED}{Style.NORMAL}Timeout expired for {proj_bug}{Style.RESET_ALL}"
                 )
-                continue
             except subprocess.CalledProcessError as e:
                 continue
-            if not store_patches:
+            if "tbar" in run_option:
                 time_spent = time.time() - start
-                print(f"Time spent: {time_spent}")
                 fields = [proj_bug, time_spent]
                 with open(f"{current_path}/patches/{time_file}.csv", "a") as f:
                     writer = csv.writer(f)
                     writer.writerow(fields)
                 print(
-                    f"{Fore.GREEN}{Style.BRIGHT}Time spent to fix bug: {time_spent}{Style.RESET_ALL}"
+                    f"{Fore.CYAN}{Style.NORMAL}Time spent to fix bug: {time_spent}{Style.RESET_ALL}"
                 )
