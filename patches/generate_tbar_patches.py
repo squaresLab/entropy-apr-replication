64b6495a4a4cadbbd53836abda499390e37bac88
aidanyang <aidanbyang@hotmail.com>
2024-02-23 20:40:22 -0500

    Switch to fixed bugs

--- patches/generate_tbar_patches.py
+++ patches/generate_tbar_patches.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     current_path = os.getcwd()
     tbar_directory = f"{current_path}/TBar"
-    patch_directory = f"{tbar_directory}/Results/PerfectFL/TBar"
+    patch_directory = f"{tbar_directory}/Results/PerfectFL/TBar/FixedBugs"
     output_fixed = f"{tbar_directory}/OUTPUT/PerfectFL/TBar/FixedBugs"
     output_partial = f"{tbar_directory}/OUTPUT/PerfectFL/TBar/PartiallyFixedBugs"
     output_unfix = f"{tbar_directory}/OUTPUT/PerfectFL/TBar/UnfixedBugs"
